0ee6b4319aa4de4991bba68f4fa818e0f1142488
khadijaludhi <khadijaludhi@gmail.com>
2023-08-27 18:42:30 +0100

    updated script.js so colours are stored

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Selecting important elements from the HTML document.
 const container = document.getElementById("grid-container");
 const gridSizeSlider = document.getElementById("gridSize");
 const gridSizeValue = document.getElementById("gridSizeValue");
@@ -5,83 +6,146 @@ const gridSizeValue2 = document.getElementById("gridSizeValue2");
 const resetButton = document.getElementById("resetButton");
 const penColorPicker = document.getElementById("penColor");
 const backgroundColorPicker = document.getElementById("backgroundColor");
+const undoButton = document.getElementById("undoButton");
+const redoButton = document.getElementById("redoButton");
 
-let isMousePressed = false;
-let currentPenColor = '#000000'; // Default color
+// Setting initial state and default values.
+let isMousePressed = false; // Whether the mouse is being pressed or not.
+let currentPenColor = '#000000'; // Default drawing color.
+let drawingSession = false; // Track if user is currently drawing.
 
+// For undo and redo functionality.
+let history = []; // To keep track of drawn states.
+let historyIndex = -1; // To navigate through drawn states.
+
+// Save the current drawing to history.
+function saveStateToHistory() {
+    const currentState = [...container.children].map(cell => ({
+        color: cell.style.backgroundColor,
+        painted: cell.hasAttribute('painted')
+    }));
+    history = history.slice(0, historyIndex + 1);
+    history.push(currentState);
+    historyIndex++;
+}
+
+// Load a saved state from history.
+function loadStateFromHistory() {
+    const savedState = history[historyIndex];
+    
+    container.childNodes.forEach((cell, index) => {
+        cell.style.backgroundColor = savedState[index].color;
+        
+        if (savedState[index].painted) {
+            cell.setAttribute('painted', true);
+        } else {
+            cell.removeAttribute('painted');
+        }
+    });
+}
+
+// Create a grid for drawing.
 function createGrid(n) {
-    container.innerHTML = ''; // Clear existing grid
-    container.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+    container.innerHTML = ''; // Remove any previous grid.
+    container.style.gridTemplateColumns = `repeat(${n}, 1fr)`; // Set grid columns.
 
     for (let i = 0; i < n * n; i++) {
         const cell = document.createElement("div");
         cell.classList.add("grid-cell");
         
-        // Attach mouse events to each cell for drawing functionality
+        // Event to handle starting a drawing.
         cell.addEventListener('mousedown', function(event) {
             isMousePressed = true;
             event.target.style.backgroundColor = currentPenColor;
-            event.target.setAttribute('painted', true); // Tag cell as painted
+            event.target.setAttribute('painted', true);
+            drawingSession = true;  // Indicate that drawing has started.
         });
-
+        
+        // Event to handle drawing continuation.
         cell.addEventListener('mouseover', function(event) {
             if (isMousePressed) {
                 event.target.style.backgroundColor = currentPenColor;
-                event.target.setAttribute('painted', true); // Tag cell as painted
+                event.target.setAttribute('painted', true);
             }
         });
-        
-        container.appendChild(cell);
+
+        container.appendChild(cell); // Add cell to the grid.
     }
 }
 
+// Event to handle grid size change.
 gridSizeSlider.addEventListener('input', function() {
     gridSizeValue.textContent = this.value;
     gridSizeValue2.textContent = this.value;
     createGrid(this.value);
+    saveStateToHistory();
 });
 
+// Event to handle grid reset.
 resetButton.addEventListener('click', function() {
     gridSizeSlider.value = 16;
     gridSizeValue.textContent = 16;
     gridSizeValue2.textContent = 16;
     createGrid(16);
-    
-    // Reset colors and 'painted' attribute for all cells
-    let cells = document.querySelectorAll('.grid-cell');
-    cells.forEach(cell => {
-        cell.style.backgroundColor = '#ffffff'; // Set background color to white
-        cell.removeAttribute('painted'); // Remove painted attribute
-    });
-    
-    penColorPicker.value = '#000000'; // Reset pen color to black
-    backgroundColorPicker.value = '#ffffff'; // Reset background color to white
-    currentPenColor = '#000000'; // Set the current pen color to the default
+    penColorPicker.value = '#000000';
+    backgroundColorPicker.value = '#ffffff'; 
+    currentPenColor = '#000000';
+    saveStateToHistory();
 });
 
+// Event to update drawing color.
 penColorPicker.addEventListener('input', function() {
     currentPenColor = this.value;
 });
 
+// Event to update grid background color.
 backgroundColorPicker.addEventListener('input', function() {
     let cells = document.querySelectorAll('.grid-cell');
     cells.forEach(cell => {
-        // Only change the background color of cells which have not been painted over
         if (!cell.hasAttribute('painted')) {
             cell.style.backgroundColor = this.value;
         }
     });
+    saveStateToHistory();
 });
 
-// Global mouse events to handle drawing and prevent unwanted behavior
+// Undo functionality.
+undoButton.addEventListener('click', function() {
+    if (historyIndex > 0) {
+        historyIndex--;
+        loadStateFromHistory();
+    }
+});
+
+// Redo functionality.
+redoButton.addEventListener('click', function() {
+    if (historyIndex < history.length - 1) {
+        historyIndex++;
+        loadStateFromHistory();
+    }
+});
+
+// Events to handle end of drawing session.
 document.addEventListener('mouseup', function() {
     isMousePressed = false;
+    if (drawingSession) {
+        drawingSession = false;
+        saveStateToHistory();
+    }
 });
 document.addEventListener('mouseleave', function() {
     isMousePressed = false;
+    if (drawingSession) {
+        drawingSession = false;
+        saveStateToHistory();
+    }
 });
+
+// Prevent unwanted drag behavior.
 document.addEventListener('dragstart', function(event) {
     event.preventDefault();
 });
 
-createGrid(16);  // Create the initial grid
+// Create initial grid and save its state.
+createGrid(16);
+saveStateToHistory(); 
