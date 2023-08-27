0efb568493b6f7650a761f37eb2ae869c55278c1
khadijaludhi <khadijaludhi@gmail.com>
2023-08-27 14:12:31 +0100

    reset button

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const container = document.getElementById("grid-container");
 const gridSizeSlider = document.getElementById("gridSize");
 const gridSizeValue = document.getElementById("gridSizeValue");
 const gridSizeValue2 = document.getElementById("gridSizeValue2");
+const resetButton = document.getElementById("resetButton");
 
-function createGrid(rows, cols) {
+function createGrid(n) {
     container.innerHTML = ''; // Clear existing grid
-    container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
-    for (let i = 0; i < rows * cols; i++) {
+    container.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+
+    for (let i = 0; i < n * n; i++) {
         const cell = document.createElement("div");
         cell.classList.add("grid-cell");
         container.appendChild(cell);
@@ -16,7 +18,14 @@ function createGrid(rows, cols) {
 gridSizeSlider.addEventListener('input', function() {
     gridSizeValue.textContent = this.value;
     gridSizeValue2.textContent = this.value;
-    createGrid(this.value, this.value);
+    createGrid(this.value);
 });
 
-createGrid(16, 16);  // Initial grid
+resetButton.addEventListener('click', function() {
+    gridSizeSlider.value = 16;
+    gridSizeValue.textContent = 16;
+    gridSizeValue2.textContent = 16;
+    createGrid(16);
+});
+
+createGrid(16);  // Initial grid
